f0f4dd0c104fc497cc6266e48de57c0d854dae46
Vedank Singh Kirola <t-vedank.kirola@ocltp.com>
2025-06-17 13:09:48 +0530

    Ec2 using Boto3

--- Ec2_Boto.py
+++ Ec2_Boto.py
@@ -1,9 +1,11 @@
 import boto3
-session = Ses
-ec2_console = boto3.client('ec2')
 
-response = ec2_console.run_instances(
-    ImageId='ami-09e6f87a47903347c',
+# Create EC2 client 
+ec2_client = boto3.client('ec2', region_name='us-east-1')  
+
+# Launch instance
+response = ec2_client.run_instances(
+    ImageId='ami-09e6f87a47903347c',  
     InstanceType='t2.micro',
     MinCount=1,
     MaxCount=1,
@@ -16,4 +18,17 @@ response = ec2_console.run_instances(
         }
     ]
 )
+
+# Get the instance ID
+instance_id = response['Instances'][0]['InstanceId']
+print("Instance ID:", instance_id)
+
+# Wait until it's running
+ec2_resource = boto3.resource('ec2', region_name='us-east-1')
+instance = ec2_resource.Instance(instance_id)
+instance.wait_until_running()
+
+# Reload instance data to get public IP
+instance.load()
+print("Public IPv4:", instance.public_ip_address)
 
